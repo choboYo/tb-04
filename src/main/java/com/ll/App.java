3fdbc1a2360cdef7da743f302e65715a7bc4429a
choboYo <tjddlsmn96@gmail.com>
2024-04-23 16:22:47 +0900

    2단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,30 @@
 package com.ll;
 
+import java.util.Scanner;
+
 class App {
     void run() {
-        System.out.println("== 프로그램 실행 ==");
+        System.out.println("== 명언 앱 ==");
 
+
+        while (true) {
+            System.out.println("명령) ");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+            else if (cmd.equals("등록")) {
+                System.out.print("작가 : ");
+                String AthoreName = scanner.nextLine();
+
+                System.out.print("명언 : ");
+                String WiseAnswer = scanner.nextLine();
+
+                System.out.printf("작가 : %s 명언 : %s\n", AthoreName, WiseAnswer);
+            }
+        }
     }
 }
