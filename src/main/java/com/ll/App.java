2b48cceb67c2feba415c20e2a87aaf62b395da08
choboYo <tjddlsmn96@gmail.com>
2024-04-25 13:52:54 +0900

    명언 삭제

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,36 +1,57 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
     void run() {
         System.out.println("== 명언 앱 ==");
         int count = 0;
+        String AthoreName = "";
+        String WiseAnswer = "";
+        List<Plus> pluses = new ArrayList<>();
 
 
         while (true) {
             System.out.println("명령) ");
-
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
+
             if (cmd.equals("종료")) {
                 break;
-            }
-            else if (cmd.equals("등록")) {
+            } else if (cmd.equals("등록")) {
                 System.out.print("작가 : ");
-                String AthoreName = scanner.nextLine();
+                AthoreName = scanner.nextLine();
+
 
                 System.out.print("명언 : ");
-                String WiseAnswer = scanner.nextLine();
+                WiseAnswer = scanner.nextLine();
 
-                if (WiseAnswer.length() != 0) {
-                    count++;
-                    int a = count;
-                }
+                count++;
+
+                Plus plus = new Plus(count, WiseAnswer, AthoreName);
+                pluses.add(plus);
 
                 System.out.printf("작가 : %s 명언 : %s\n %d번 명언이 등록되었습니다.\n", AthoreName, WiseAnswer, count);
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언 ");
+                System.out.println("-------------------");
+
+                for (int i = pluses.size() - 1; i >= 0; i--) {
+                    Plus plus = pluses.get(i);
+                    System.out.printf("%d   %s    %s\n", plus.id, plus.authorName, plus.content);
+                }
+
+            }
+            else if (cmd.substring(0,2).equals("삭제")) {
+                int IdSearch = cmd.indexOf("=");
+                int id =Integer.valueOf(cmd.substring(IdSearch + 1));
+                pluses.remove(id -1);
+
             }
         }
     }
 }
+
---
+++ src/main/java/com/ll/Plus.java
@@ -0,0 +1,14 @@
+package com.ll;
+
+class Plus {
+    int id;
+    String content;
+    String authorName;
+
+    public Plus(int id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+
+}
